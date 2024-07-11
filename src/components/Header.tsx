863d7ced4c0ee8888c54b9cafa41f66fa9af013e
oluceps <i@oluceps.uk>
2024-07-11 15:30:14 +0000

    ~ nya

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,11 +9,11 @@ const Header: Component<{ sticky: boolean }> = (props) => {
 		<header class={`${props.sticky ? "sticky" : ""} backdrop-blur-sm top-0 w-screen flex items-center justify-between mx-3 xl:mx-5 2xl:mx-6`}>
 			<A
 				href="/"
-				class="flex items-center justify-center text-lg 2xl:text-2xl font-bold px-1 rounded-sm text-sprout-650 overflow-visible ml-2"
+				class="flex items-center justify-center text-lg 2xl:text-2xl font-bold px-1 rounded-sm text-sprout-650 overflow-visible ml-2 group"
 			>
 				<div class="relative h-4 w-auto">
-					<div class="absolute h-4 w-4 -top-1.5 -start-1.5 bg-sprout-300 dark:bg-sprout-400 rounded-sm" />
-					<div class="absolute leading-none text-neutral-600 dark:text-slate-200 text-nowrap">
+					<div class="absolute h-4 w-4 -top-1.5 -start-1.5 bg-sprout-300 dark:bg-sprout-400 rounded-sm group-hover:h-6 group-hover:w-6 transition-all duration-500 group-hover:bg-sprout-200" />
+					<div class="absolute leading-none text-neutral-600 group-hover:text-neutral-500 dark:text-slate-200 text-nowrap">
 						{cfg.title}
 					</div>
 				</div>
