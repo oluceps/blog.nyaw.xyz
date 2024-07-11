45016ac3699726ce56e10446a16fd67487963676
oluceps <i@oluceps.uk>
2024-07-11 18:22:53 +0000

    ~

--- src/components/BackTopBtn.tsx
+++ src/components/BackTopBtn.tsx
@@ -13,7 +13,7 @@ const ScrollTopBtn: Component = () => {
         window.scrollTo({ top: 0, behavior: "smooth" });
       }} class={`!fixed bottom-5 right-5 p-3 mb-3 bg-sprout-200/60 hover:bg-sprout-200/90 backdrop-blur-md rounded-md h-9 w-9 grid items-center justify-center
         transition-all duration-500 hover:h-11 hover:w-11
-        ${document.documentElement.scrollHeight - document.documentElement.clientHeight - scroll.y < 200 ? "[&:not(:hover)]:motion-safe:animate-bounce" : ""}`}>
+        ${document.documentElement.scrollHeight - document.documentElement.clientHeight - scroll.y < 200 ? "[&:not(:hover)]:motion-safe:animate-pulse bg-sprout-300/75" : ""}`}>
         <Icon path={chevronUp} class={`h-full w-full`} />
       </button>
     </Show>
