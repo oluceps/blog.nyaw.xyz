b158e7980dfc55e70b4193d18b6c2c7bcddc0944
oluceps <i@oluceps.uk>
2024-07-11 15:16:11 +0000

    +

--- src/components/BackTopBtn.tsx
+++ src/components/BackTopBtn.tsx
@@ -6,21 +6,14 @@ import {
   chevronUp
 } from "solid-heroicons/solid";
 const ScrollTopBtn: Component = () => {
-  const [hiddenState, setHiddenState] = createSignal(true)
   const scroll = useWindowScrollPosition();
-  createEffect(() => {
-    if (scroll.y > 350) {
-      setHiddenState(false)
-    } else {
-      setHiddenState(true)
-    }
-  })
   return (<>
-    <Show when={!hiddenState() && (document.documentElement.clientWidth > 930)}>
+    <Show when={(scroll.y > 350) && (document.documentElement.clientWidth > 930)}>
       <button onclick={() => {
         window.scrollTo({ top: 0, behavior: "smooth" });
-      }} class={`!fixed bottom-5 right-5 p-3 mb-3 bg-sprout-200/60 hover:bg-sprout-200/90 backdrop-blur-md rounded-md h-9 w-9 grid items-center justify-center}`}>
-        <Icon path={chevronUp} class="h-full w-full" />
+      }} class={`!fixed bottom-5 right-5 p-3 mb-3 bg-sprout-200/60 hover:bg-sprout-200/90 backdrop-blur-md rounded-md h-9 w-9 grid items-center justify-center
+        ${document.documentElement.scrollHeight - document.documentElement.clientHeight - scroll.y < 200 ? "animate-shaking" : ""}`}>
+        <Icon path={chevronUp} class={`h-full w-full`} />
       </button>
     </Show>
   </>
