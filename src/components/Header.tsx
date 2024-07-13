22eb215dd2d73547e28ccb026a93687daf38c764
oluceps <i@oluceps.uk>
2024-07-13 07:13:03 +0000

    =

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ const Header: Component<{ sticky: boolean }> = (props) => {
 				class="flex items-center justify-center text-lg 2xl:text-2xl font-bold px-1 rounded-sm text-sprout-650 overflow-visible ml-2 group"
 			>
 				<div class="relative h-4 w-auto">
-					<div class="absolute h-4 w-4 -top-1.5 -left-1.5 group-hover:top-1.5 group-hover:left-1.5 bg-sprout-300 dark:bg-sprout-400 rounded-sm transition-all duration-500 group-hover:bg-sprout-200 transform-gpu" />
+					<div class="absolute h-4 w-4 -top-1.5 -left-1.5 group-hover:top-1.5 group-hover:left-1.5 bg-sprout-300 dark:bg-sprout-400 rounded-sm transition-all duration-300 group-hover:bg-sprout-200 transform-gpu" />
 					<div class="absolute leading-none text-neutral-600 dark:text-slate-200 text-nowrap">
 						{cfg.title}
 					</div>
