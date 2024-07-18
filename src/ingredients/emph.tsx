5370000c84515f867deb7c8557be87718593f10f
oluceps <i@oluceps.uk>
2024-07-18 19:02:57 +0000

    -

--- src/ingredients/emph.tsx
+++ src/ingredients/emph.tsx
@@ -8,6 +8,7 @@ import {
   heart,               // tip
   bellAlert            // notice
 } from "solid-heroicons/solid";
+import { Dynamic } from "solid-js/web";
 
 export const isExternalURL = (url: string) => /^https?:\/\//.test(url);
 
@@ -24,25 +25,36 @@ const icons = {
 };
 
 export const Emph: ParentComponent<EmphProps> = (props) => {
-  const exp = () => {
-    switch (props.type) {
-      case "warn": return ["red", "WARNING"]
-      case "info": return ["sprout", "Information"]
-      case "tips": return ["chill", "Tips"]
-      case "note": return ["ouchi", "Notice"]
-    }
+
+  const w = () => <Icon path={icons[props.type]} class={`h-7 w-7 fill-red-400`} />
+  const i = () => <Icon path={icons[props.type]} class={`h-7 w-7 fill-sprout-400`} />
+  const t = () => <Icon path={icons[props.type]} class={`h-7 w-7 fill-chill-400`} />
+  const n = () => <Icon path={icons[props.type]} class={`h-7 w-7 fill-ouchi-400`} />
+
+  const iconOpts = {
+    warn: w,
+    info: i,
+    tips: t,
+    note: n
   }
 
-  const col = () => exp()[0];
+  const ex = () => {
+    switch (props.type) {
+      case "warn": return "WARNING"
+      case "info": return "Information"
+      case "tips": return "Tips"
+      case "note": return "Notice"
+    }
+  }
 
   return (
     <div class={`group relative rounded-xl border-4 border-sprout-300/80`}>
       <div class="absolute -inset-px rounded-xl border-2 opacity-0" />
       <div class="relative overflow-hidden rounded-xl px-5 py-4">
         <div class="flex items-center">
-          <Icon path={icons[props.type]} class={`h-7 w-7 fill-${col()}-400`} />
+          <Dynamic component={iconOpts[props.type]} />
           <div class="text-xl text-slate-900 dark:text-white capitalize no-underline pl-3">
-            {exp()[1]}
+            {ex()}
           </div>
         </div>
         <p class="text-[0.91rem] pl-1 text-balance text-slate-800 dark:text-slate-300 -mb-2">
