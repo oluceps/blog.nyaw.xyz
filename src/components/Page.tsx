59989eb650af3c10bfb9af56dd63475d5d18b137
oluceps <i@oluceps.uk>
2024-07-31 15:19:45 +0800

    +

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,10 +1,9 @@
 import { Link, Meta, MetaProvider, Title } from "@solidjs/meta";
-import { Show, ParentComponent, children, createSignal, createEffect, createMemo } from "solid-js";
+import { Show, ParentComponent, children, createSignal, createEffect, createMemo, onMount } from "solid-js";
 import cfg from "../constant";
 import data from "../routes/data.json"
 import { useLocation } from "@solidjs/router";
 import { TableOfContents } from "./Toc";
-import BackTopBtn from "./BackTopBtn";
 
 function formatDate(date: Date | undefined) {
 	if (date === undefined) {
@@ -22,8 +21,9 @@ const Page: ParentComponent<{ isError?: false }> = (props) => {
 	const resolved = children(() => props.children)
 	const location = useLocation();
 	const [currentPath, setCurrentPath] = createSignal(location.pathname);
-	createEffect(() => {
-		setCurrentPath(location.pathname);
+	console.log(currentPath())
+	onMount(() => {
+		setCurrentPath(currentPath().substring(1));
 	});
 
 	const [ctx] = createSignal(
@@ -32,7 +32,8 @@ const Page: ParentComponent<{ isError?: false }> = (props) => {
 
 	const currentUrl = `${cfg.base_url}${location.pathname}`
 
-	const article = createMemo(() => ctx().find((i) => i.path === currentPath().substring(1)))
+	const article = createMemo(() => ctx().find((i) => i.path === currentPath()))
+	console.log("article", article())
 	return (
 		<MetaProvider>
 			<Title>{`${article()?.title} - ${cfg.title}`}</Title>
