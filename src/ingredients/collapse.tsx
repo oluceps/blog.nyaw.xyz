d5df20d564f1f7e3d49acfdca52246858384f862
oluceps <i@oluceps.uk>
2024-07-31 17:47:14 +0800

    ~

--- src/ingredients/collapse.tsx
+++ src/ingredients/collapse.tsx
@@ -10,8 +10,7 @@ const Col: Component<ParentProps & { title: string }> =
 
     return (
       <div class="rounded-md ring ring-sprout-200 p-3 mt-3"
-        onmouseenter={() => setExpandState(!expandState())}
-        onmouseleave={() => setExpandState(!expandState())}
+        onClick={() => setExpandState(!expandState())}
       >
         <section class="CollapseContainer">
           <div class="flex justify-between items-center"><div class={`font-bold ${expandState() ? "text-sprout-600" : "text-slate-500"}`}>{props.title}</div><Icon path={plus} class={`w-4 h-4 mr-3 transition-all duration-500 transform-gpu ${expandState() ? "rotate-45" : ""}`} /></div>
