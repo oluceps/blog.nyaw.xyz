d69b3a8dd89192d803b296f8b65ec94e5257e093
oluceps <i@oluceps.uk>
2024-08-10 06:32:46 +0800

    -

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer: Component = () => {
 	return (
 		<div class="relative bottom-0 w-full justify-end text-[10px] group flex-nowrap">
 			<div class="group-hover:block hidden"><div class="flex justify-end items-center mr-3 space-x-1 flex-nowrap">
-				<p>BUILT ON {(new Date(time)).toLocaleString()}</p>
+				<p>BUILT ON {time}</p>
 			</div>
 			</div>
 			<div class="group-hover:hidden flex justify-end items-center mr-3 space-x-1 flex-nowrap">©{valid} {cfg.author} | {cfg.license}</div>
