9555e74a5f0040892f58ff9a5e0d9160382193bd
oluceps <i@oluceps.uk>
2024-08-11 13:30:51 +0800

    +

---
+++ src/ingredients/post-scroll.tsx
@@ -0,0 +1,21 @@
+
+import { Show, type Component, type ParentProps } from "solid-js"
+
+import { useWindowScrollPosition } from "@solid-primitives/scroll";
+
+
+const PostScoll: Component<ParentProps> =
+  (props) => {
+    const scroll = useWindowScrollPosition();
+    return (
+      <>
+        <div class={scroll.y < 20 ? "h-48" : "h-4"} />
+        <Show when={scroll.y > 20}>
+          {props.children}
+        </Show>
+      </>
+    )
+  }
+
+
+export default PostScoll;
