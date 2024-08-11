15bb91d8ea2f985b3fc2817f5bcdef5ca94f9055
oluceps <i@oluceps.uk>
2024-08-11 13:33:15 +0800

    -

--- src/ingredients/quick-link.tsx
+++ src/ingredients/quick-link.tsx
@@ -7,7 +7,7 @@ import { AiOutlineNumber } from "solid-icons/ai";
 import { SiMaildotru, SiMisskey } from "solid-icons/si";
 import { FaBrandsDiscord } from "solid-icons/fa";
 
-export const isExternalURL = (url: string) => /^https?:\/\//.test(url);
+export const isExternalURL = (url: string) => /^https?:\/\//.test(url) || url.includes("mailto");
 
 export type QuickLinksProps = {
   title: string;
