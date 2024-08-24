c35410b259e7e7078f8ce8da69b3b6e313f61a81
oluceps <i@oluceps.uk>
2024-08-25 01:10:32 +0800

    -

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,9 +1,9 @@
 import { Link, Meta, Title } from "@solidjs/meta";
 import { Show, type ParentComponent, children, createMemo } from "solid-js";
 import cfg from "../constant";
-import data from "../routes/data.json";
 import { useLocation } from "@solidjs/router";
 import { TableOfContents } from "./Toc";
+import { ctxFiltered } from "./Arti";
 
 function formatDate(date: Date | undefined) {
 	if (date === undefined) {
@@ -21,9 +21,7 @@ const Page: ParentComponent<{ isError?: false }> = (props) => {
 	const resolved = children(() => props.children);
 	const location = useLocation();
 
-	const ctx = data.map((i) => {
-		return { ...i, date: new Date(i.date) };
-	});
+	const ctx = ctxFiltered;
 
 	const article = createMemo(() =>
 		ctx.find((i) => i.path == location.pathname.replaceAll("/", "")),
@@ -46,22 +44,20 @@ const Page: ParentComponent<{ isError?: false }> = (props) => {
 				property="article:published_time"
 				content={formatDate(article()?.date)}
 			/>
-			<Show when={!article()?.noBanner}>
-				<h1>{article()?.title}</h1>
-				<div class="text-zinc-500 font-serif mb-2 font-light text-sm 2xl:text-lg">
-					{formatDate(article()?.date)}
-				</div>
+			<h1>{article()?.title}</h1>
+			<div class="text-zinc-500 font-serif mb-2 font-light text-sm 2xl:text-lg">
+				{formatDate(article()?.date)}
+			</div>
 
-				<div class="flex w-auto mb-10 justify-end items-end">
-					<Show when={1}>
-						<i class="text-pretty text-slate-500 text-start text-sm 2xl:text-lg font-mono leading-loose">
-							{article()?.description}
-						</i>
-					</Show>
-				</div>
-				<Show when={article()?.toc}>
-					<TableOfContents children={resolved()} />
+			<div class="flex w-auto mb-10 justify-end items-end">
+				<Show when={1}>
+					<i class="text-pretty text-slate-500 text-start text-sm 2xl:text-lg font-mono leading-loose">
+						{article()?.description}
+					</i>
 				</Show>
+			</div>
+			<Show when={article()?.toc}>
+				<TableOfContents children={resolved()} />
 			</Show>
 			{resolved()}
 		</article>
