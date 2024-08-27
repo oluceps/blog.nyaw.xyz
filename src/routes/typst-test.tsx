fc3b1c83af2514b5e46d70f5dbed2705500d6b7d
oluceps <i@oluceps.uk>
2024-08-28 03:33:07 +0800

    +

--- src/routes/typst-test.tsx
+++ src/routes/typst-test.tsx
@@ -1,9 +1,12 @@
 import { createSignal } from "solid-js";
 import { TypstDocument } from "../lib/TypstDocument"
 
-export function Typstest() {
+export default function Typstest() {
 
   const str = "Hello, World!Hello, World!Hello, World!Hello, World!Hello, World!Hello, World!Hello, World!";
   const [artifact] = createSignal(new Uint8Array(str as unknown as number));
-  return (<div class="w-full h-full"><TypstDocument fill="#343541" artifact={artifact()} /></div>)
+  return (<div class="w-full h-full justify-center items-center flex flex-col">
+    This is for testing the [WIP] typst solidjs component
+    <TypstDocument fill="#343541" artifact={artifact()} />
+  </div>)
 }
