d1562169e7b90d3d583f267abede0b4fe7496a0e
oluceps <i@oluceps.uk>
2024-08-29 03:25:48 +0800

    -

--- src/components/BackTopBtn.tsx
+++ src/components/BackTopBtn.tsx
@@ -14,9 +14,8 @@ const ScrollTopBtn: Component = () => {
 						window.scrollTo({ top: 0, behavior: "smooth" });
 					}}
 					class={twMerge(
-						`!fixed bottom-5 right-5 p-3 mb-3 bg-sprout-200/60 backdrop-blur-md rounded-md h-9 w-9 grid items-center justify-center
-        transition-all duration-300 hover:h-11 hover:w-11`,
-						`${document.documentElement.scrollHeight - document.documentElement.clientHeight - scroll.y < 200 ? "[&:not(:hover)]:motion-safe:animate-pulse bg-sprout-300/75 h-10 w-10" : ""}`,
+						`!fixed bottom-5 right-5 p-3 mb-3 bg-sprout-200/60 backdrop-blur-md rounded-md h-9 w-9 grid items-center justify-center`,
+						`transition-all duration-200 hover:h-11 hover:w-11 bg-sprout-300/75 h-10 w-10`,
 					)}
 				>
 					<Icon path={chevronUp} class={`h-full w-full`} />
