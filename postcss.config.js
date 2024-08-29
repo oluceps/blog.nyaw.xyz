0a6cc155203908946962ed0d0a23d26e5151aea7
oluceps <i@oluceps.uk>
2024-08-30 00:21:36 +0800

    +

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,13 @@ export default {
 	plugins: {
 		tailwindcss: {},
 		autoprefixer: {},
-		...(process.env.NODE_ENV === 'production' ? { cssnano: { preset: 'default' } } : {})
+		...(process.env.NODE_ENV === 'production' ? {
+			cssnano: { preset: 'default' },
+			'@fullhuman/postcss-purgecss': {
+				content: [
+					'./src/**/*.{js,jsx,ts,tsx}',
+				],
+			}
+		} : {})
 	},
 };
