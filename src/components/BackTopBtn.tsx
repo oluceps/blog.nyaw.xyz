7afda55807ca97bc2682003cd6940cf4cb6cae14
oluceps <i@oluceps.uk>
2024-08-29 21:37:08 +0800

    =

--- src/components/BackTopBtn.tsx
+++ src/components/BackTopBtn.tsx
@@ -11,7 +11,7 @@ const ScrollTopBtn: Component = () => {
 					onclick={() => {
 						window.scrollTo({ top: 0, behavior: "smooth" });
 					}}
-					class="!fixed bottom-5 right-5 p-3 mb-3 bg-sprout-200/60 rounded-md h-9 w-9 grid items-center justify-center duration-200 bg-sprout-300/75 h-10 w-10 lg:h-12 lg:w-12"
+					class="!fixed bottom-5 right-5 p-3 mb-3 backdrop-blur-md bg-sprout-200/60 rounded-md h-9 w-9 grid items-center justify-center duration-200 bg-sprout-300/75 h-10 w-10 lg:h-12 lg:w-12"
 				>
 					<Icon path={chevronUp} class={`h-full w-full`} />
 				</button>
