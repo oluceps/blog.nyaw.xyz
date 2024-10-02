f1ce80fbe76fdb5060b5c90a660c8ea530ae926e
oluceps <secinia@outlook.com>
2024-10-03 04:59:48 +0800

    +

--- src/routes/api/tier.tsx
+++ src/routes/api/tier.tsx
@@ -2,5 +2,8 @@ import { geolocation } from '@vercel/functions';
 
 export function GET(request: Request) {
 	const { country } = geolocation(request);
-	return country === "CN" ? 1 : 0;
+	// return country === "CN" ? 1 : 0;
+	return new Response((country === "CN" ? 1 : 0).toString(), {
+		headers: { 'content-type': 'application/json' },
+	});
 }
