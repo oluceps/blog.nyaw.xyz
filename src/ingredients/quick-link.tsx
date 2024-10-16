15228c3a8afa9f4c081501466b28510298a245d4
oluceps <secinia@outlook.com>
2024-10-17 02:33:34 +0800

    +

--- src/ingredients/quick-link.tsx
+++ src/ingredients/quick-link.tsx
@@ -1,4 +1,4 @@
-import { createSignal, type JSXElement, onCleanup, type ParentComponent, Show } from "solid-js";
+import { createMemo, createSignal, type JSXElement, onCleanup, type ParentComponent, Show } from "solid-js";
 
 import { Dynamic } from "solid-js/web";
 import { twMerge } from "tailwind-merge";
@@ -18,43 +18,44 @@ export type QuickLinksProps = {
 
 export const QuickLinks: ParentComponent<QuickLinksProps> = (props) => {
 	const [isShow, setIsRelative] = createSignal(false);
+	const [innerW, setInnerW] = createSignal(0);
 	let timeoutId: any;
+	let intervalId: any;
 	const handleMouseEnter = () => {
 		timeoutId = setTimeout(() => {
 			setIsRelative(true);
 		}, 450);
+		intervalId = setInterval(() => {
+			setInnerW(prev => prev < 100 ? prev + 1 : 100)
+			console.log(innerW())
+		}, 50)
 	};
 
 	const handleMouseLeave = () => {
 		clearTimeout(timeoutId);
 		setIsRelative(false);
+		clearTimeout(intervalId);
+		setInnerW(0);
 	};
+
+	// const innerWText = createMemo(() => `w-[${innerW()}%]`);
 
 	onCleanup(() => clearTimeout(timeoutId));
 
+	// <a href={props.href} target="_blank">
+	// 	<span class="absolute -inset-px rounded-xl" />
+	// </a>
 	return (
-		<div class="group relative rounded-xl shadow-md border" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+		<div class="group relative not-prose rounded-xl shadow-md border" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
 			<div class="absolute -inset-px rounded-xl border-transparent opacity-0 transition-all [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.sprout.100)),var(--quick-links-hover-bg,theme(colors.sprout.100)))_padding-box,linear-gradient(to_top,theme(colors.sprout.500),theme(colors.sprout.300))_border-box] group-hover:opacity-80" />
 
 			<div class="relative overflow-hidden h-full w-full grow">
-				<a href={props.href} target="_blank">
-					<span class="absolute -inset-px rounded-xl" />
-				</a>
-				<div class="m-4">
-					<div class="flex w-full grow justify-center items-center overflow-visible">
-						<div class="inline-flex justify-between items-center">
-							<Dynamic component={() => props.icon} />
-							<Show when={!props.onlyIcon}>
-								<div class="group">
-									<p
-										class={twMerge(isShow() ? 'inline-flex' : 'hidden', "whitespace-nowrap pointer-events-none select-none shrink grow justify-end pl-4 items-center no-underline font-semibold bg-gradient-to-br from-sprout-400 to-sprout-700 bg-clip-text text-lg md:text-normal w-full text-transparent")}
-									>
-										{props.title}
-									</p>
-								</div>
-							</Show>
-						</div>
+				<div class="flex items-center justify-center m-4">
+					<div class="">
+						<Dynamic component={() => props.icon} />
 					</div>
+
+
 				</div>
 			</div>
 		</div>
