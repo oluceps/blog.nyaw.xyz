3f8abb9612b5fb6d7723a635aa4f762eb71ab352
oluceps <secinia@outlook.com>
2024-10-27 00:07:28 +0800

    +

--- src/routes/api/donate.tsx
+++ src/routes/api/donate.tsx
@@ -2,9 +2,18 @@ import type { APIEvent } from "@solidjs/start/server";
 
 export function GET(_event: APIEvent) {
 	const addr = {
-		"Monero": "46TckNysSV7PdNA3PW134heWh4Nv3Fxv7Lab7aqw6utMEFK5qJypgteRZppWfsYnZAeoi7sddUP41Sr2YfFqr4urBiE1Xbw",
-		"Solana": "AqRNAHbp12wk6HMSd8cffFz1NXXigBiU14LLecP4Rkxo",
-		"ERC-20": "0xAD109D5E5CE7EF7F6263AA5238d6fb1b40516e68"
+		"Monero": {
+			address: "46TckNysSV7PdNA3PW134heWh4Nv3Fxv7Lab7aqw6utMEFK5qJypgteRZppWfsYnZAeoi7sddUP41Sr2YfFqr4urBiE1Xbw",
+			blake3_hash: "625a890df3403600c73a84f6153c191490de600625e6ca6dd8acaa3f58b92423"
+		},
+		"Solana": {
+			address: "AqRNAHbp12wk6HMSd8cffFz1NXXigBiU14LLecP4Rkxo",
+			blake3_hash: "4e7d49734ffb6e38e52696f815b2fa5072211ef3c79f4d26f498a4a5091d2cd3"
+		},
+		"ERC-20": {
+			address: "0xAD109D5E5CE7EF7F6263AA5238d6fb1b40516e68",
+			blake3_hash: "d53b982a1e938f5615393795e0ba0b390950fdd9e7c924657dd8ff63d8366447"
+		}
 	}
 	return new Response(JSON.stringify(addr), {
 		headers: { "content-type": "application/json" },
