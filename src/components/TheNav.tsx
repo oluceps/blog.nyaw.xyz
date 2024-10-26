93a6d6555ca2e7b53aeb448fde3afd66faf6c5cd
oluceps <secinia@outlook.com>
2024-10-27 01:52:11 +0800

    ~

--- src/components/TheNav.tsx
+++ src/components/TheNav.tsx
@@ -1,5 +1,5 @@
 import { cache, createAsync, useNavigate } from "@solidjs/router";
-import { createEffect, createSignal } from "solid-js";
+import { createEffect, createSignal, For, Show } from "solid-js";
 import { Motion, Presence } from "solid-motionone";
 import cfg from "../constant";
 import { useLocation } from "@solidjs/router";
@@ -7,11 +7,6 @@ import { twMerge } from "tailwind-merge";
 
 export default function Home() {
 	const menu = cfg.menu;
-
-	// if (limit()) {
-	// 	menu.splice(0, 1);
-	// }
-
 	const navigate = useNavigate();
 
 	type TabRef = HTMLButtonElement | null;
@@ -33,32 +28,34 @@ export default function Home() {
 			}}
 			class={`bg-background flex items-center justify-end relative px-2 py-1.5`}
 		>
-			{menu.map((tab, idx) => (
-				<button
-					ref={(el) => {
-						tabRefs()[idx] = el;
-					}}
-					class={twMerge(
-						`bg-transparent font-sans px-1.5 py-1 z-10 text-neutral-500 hover:text-neutral-600 rounded-md transition ease-in delay-200`,
-						useLocation().pathname == tab.url &&
-							!hoveredTab() &&
-							useLocation().pathname.length != 1
-							? "bg-sprout-100 text-neutral-700/80"
-							: "",
-					)}
-					onpointerenter={() => {
-						setHoveredIdx(idx);
-					}}
-					onclick={() =>
-						tab.url.startsWith("/")
-							? navigate(tab.url)
-							: window.open(tab.url, "_blank")
-					}
-				>
-					{tab.name}
-				</button>
-			))}
-			{hoveredTab() ? (
+			<For each={menu}>
+				{(tab, idx) => (
+					<button
+						ref={(el) => {
+							tabRefs()[idx()] = el;
+						}}
+						class={twMerge(
+							`bg-transparent font-sans px-1.5 py-1 z-10 text-neutral-500 hover:text-neutral-600 rounded-md transition ease-in delay-200`,
+							useLocation().pathname == tab.url &&
+								!hoveredTab() &&
+								useLocation().pathname.length != 1
+								? "bg-sprout-100 text-neutral-700/80"
+								: "",
+						)}
+						onpointerenter={() => {
+							setHoveredIdx(idx);
+						}}
+						onclick={() =>
+							tab.url.startsWith("/")
+								? navigate(tab.url)
+								: window.open(tab.url, "_blank")
+						}
+					>
+						{tab.name}
+					</button>
+				)}
+			</For>
+			<Show when={hoveredTab()}>
 				<Presence>
 					<Motion.button
 						class="absolute top-0 right-0 bg-sprout-200/90 rounded-md"
@@ -87,7 +84,7 @@ export default function Home() {
 						}}
 					/>
 				</Presence>
-			) : null}
+			</Show>
 		</nav>
 	);
 }
