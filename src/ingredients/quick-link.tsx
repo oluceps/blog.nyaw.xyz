384ed6847d9e9f1f914ae57cff4f327e3804f022
oluceps <secinia@outlook.com>
2025-01-01 06:51:52 +0800

    +

--- src/ingredients/quick-link.tsx
+++ src/ingredients/quick-link.tsx
@@ -1,4 +1,4 @@
-import type { JSXElement, ParentComponent } from "solid-js";
+import { createSignal, onMount, type JSXElement, type ParentComponent } from "solid-js";
 
 import { Dynamic } from "solid-js/web";
 import { twMerge } from "tailwind-merge";
@@ -8,36 +8,42 @@ export const isExternalURL = (url: string) =>
 
 export type QuickLinksProps = {
 	title: string;
-	href: string;
+	href: string | Promise<string>;
 	icon: () => JSXElement;
 	description?: string;
 	children?: JSXElement;
 };
 
-export const QuickLinks: ParentComponent<QuickLinksProps> = (props) => (
-	<div class="group relative not-prose rounded-xl shadow-md border">
-		<div class="absolute -inset-px rounded-xl border-transparent opacity-0 transition-all [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.sprout.100)),var(--quick-links-hover-bg,theme(colors.sprout.100)))_padding-box,linear-gradient(to_top,theme(colors.sprout.500),theme(colors.sprout.300))_border-box] group-hover:opacity-80" />
+export const QuickLinks: ParentComponent<QuickLinksProps> = (props) => {
+	let [href, setHref] = createSignal('')
+	onMount(async () => {
+		setHref(props.href instanceof Promise ? await props.href : props.href)
+	})
+	return (
+		<div class="group relative not-prose rounded-xl shadow-md border">
+			<div class="absolute -inset-px rounded-xl border-transparent opacity-0 transition-all [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.sprout.100)),var(--quick-links-hover-bg,theme(colors.sprout.100)))_padding-box,linear-gradient(to_top,theme(colors.sprout.500),theme(colors.sprout.300))_border-box] group-hover:opacity-80" />
 
-		<div class="relative overflow-hidden h-full w-full grow">
-			<a href={props.href} target="_blank" rel="noreferrer">
-				<span class="absolute -inset-px rounded-xl" />
-			</a>
+			<div class="relative overflow-hidden h-full w-full grow">
+				<a onClick={() => window.open(href())} class="hover:cursor-pointer" target="_blank">
+					<span class="absolute -inset-px rounded-xl" />
+				</a>
 
-			<div class="flex items-center justify-center m-4">
-				<Dynamic component={props.icon} />
+				<div class="flex items-center justify-center m-4">
+					<Dynamic component={props.icon} />
 
-				<p
-					class={twMerge(
-						"overflow-hidden whitespace-nowrap w-0",
-						"bg-gradient-to-br from-sprout-400 to-sprout-700 bg-clip-text",
-						"pointer-events-none select-none",
-						"items-center no-underline font-semibold text-lg md:text-normal text-transparent",
-						"group-hover:w-24 flex justify-center transition-all delay-300",
-					)}
-				>
-					{props.title}
-				</p>
+					<p
+						class={twMerge(
+							"overflow-hidden whitespace-nowrap w-0",
+							"bg-gradient-to-br from-sprout-400 to-sprout-700 bg-clip-text",
+							"pointer-events-none select-none",
+							"items-center no-underline font-semibold text-lg md:text-normal text-transparent",
+							"group-hover:w-24 flex justify-center transition-all delay-300",
+						)}
+					>
+						{props.title}
+					</p>
+				</div>
 			</div>
 		</div>
-	</div>
-);
+	)
+};
