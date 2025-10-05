4c12e365960caaa45dcb522a0718e98ff40f57e6
Secirian <gh@nyaw.xyz>
2025-10-05 13:49:14 +0000

    + this init

---
+++ minisign/useRust/wasm/wasm_bg.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function setupLogging(a: number, b: number): void;
+export function __wbg_publickey_free(a: number): void;
+export function publickey_decode(a: number, b: number): number;
+export function publickey_verify(a: number, b: number, c: number, d: number): number;
+export function __wbg_signature_free(a: number): void;
+export function signature_decode(a: number, b: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_free(a: number, b: number): void;
